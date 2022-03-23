1ee6d62b286ce51a1b9c59809ba0ccda4d39934e
Sam Clark <samclark2399@gmail.com>
2022-03-22 22:54:33 -0400

    connected database to server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+{
+    host: 'localhost',
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASS}`,
+    database: 'election'
+},
+console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
