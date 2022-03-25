c341d2d0dab98f237f637034f9b4fb67ded92674
Sam Clark <samclark2399@gmail.com>
2022-03-24 22:18:12 -0400

    created router routes to modularize code.

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: `${process.env.DB_USER}`,
+        password: `${process.env.DB_PASS}`,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+    );
+    
+module.exports = db;
